27bf31ecd4d4ac5e046d0223bd1c9bb95f187bec
kangfengyuan <kangfy9@gmail.com>
2024-02-07 23:18:46 +0800

    使用Netty构建服务器端

---
+++ gateway-core/src/main/java/com/core/netty/NettyServerConnectManagerHandler.java
@@ -0,0 +1,7 @@
+package com.core.netty;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandler;
+
+public class NettyServerConnectManagerHandler extends ChannelDuplexHandler {
+}
